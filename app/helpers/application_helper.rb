a26950b33ec644cccf439bb5702943e53bb592b1
Brett Salisbury <brett.salisbury@gmail.com>
2011-06-12 18:27:35 +1000

    Added helper class to render application logo, integration tests to verify link navigation

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -9,4 +9,9 @@ module ApplicationHelper
       "#{base_title} | #{@title}"
     end
   end
+
+  # Return the html to render the application logo
+  def logo
+    image_tag("logo.png", :alt => "Sample App", :class => "round")
+  end
 end
